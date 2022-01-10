667f79a2223278bd36fe31b4a9ac1b36bed7e515
Maxim Zmeyok <maxim_zmeyok@mail.ru>
2022-01-10 23:28:45 +0300

    init: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function toReadable (number) {
-  
+  let from0To19 = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  let from0To90 = ['zero', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+  let stringOfNumber = number.toString().split('');
+
+  function transformBelow100(x) {
+    if (x < 20) {
+      return `${from0To19[x]}`;
+    } else if (x % 10 == 0) {
+      return `${from0To90[x / 10]}`;
+    } else {
+      let stringOfNumber = x.toString().split('').map(item => +item);
+      return `${from0To90[stringOfNumber[0]]} ${from0To19[stringOfNumber[1]]}`;
+    }
+  }
+
+  if (stringOfNumber.length < 3) {
+    return transformBelow100(number);
+  } else if (number % 100 == 0) {
+    return `${from0To19[Number(stringOfNumber[0])]} hundred`;
+  } else {
+    return `${from0To19[Number(stringOfNumber[0])]} hundred ${transformBelow100(number - (Math.floor(number / 100) * 100))}`;
+  }
 }
